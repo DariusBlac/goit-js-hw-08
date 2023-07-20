0277b70c17b811384ab243a0e091be8614b98061
DariusBlac <legionnsspace@gmail.com>
2023-07-20 22:02:52 +0200

    finish task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+// Перевіряємо чи є збережений час в localStorage
+
+const currentTime = localStorage.getItem(STORAGE_KEY);
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
